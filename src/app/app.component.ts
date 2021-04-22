88d98ac49d9a1ab063b6c5e11b8240d193c21d82
Hari Prasad Raju Kadumuri <hari.kadumuri@gmail.com>
2021-04-22 09:25:25 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('hari'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+
+  registrationForm = this.fb.group({
+    userName: ['hari'],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    })
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  // loadApiData() {
+  //   this.registrationForm.setValue({
+  //     userName: 'Bruce',
+  //     password: 'test',
+  //     confirmPassword: 'test',
+  //     address: {
+  //       city: 'City',
+  //       state: 'State',
+  //       postalCode: '123456'
+  //     }
+  //   })
+  // }
 }
